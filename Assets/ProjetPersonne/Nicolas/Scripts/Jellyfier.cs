0b42d71a8aa7d72fff78e7d12a743b92cdf567d8
nicolas lagree <sensisaw6@gmail.com>
2020-02-10 09:22:58 +0100

    PathFindingDemo

---
+++ Assets/ProjetPersonne/Nicolas/Scripts/Jellyfier.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Jellyfier : MonoBehaviour
+{
+    public float bounceSpeed, fallForce, stiffness;
+
+    private MeshFilter meshFilter;
+    private Mesh mesh;
+
+    JellyVertex[] jellyVertices;
+    Vector3[] currentMeshVertices;
+
+    private void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        mesh = meshFilter.mesh;
+        GetVertices();
+    }
+
+    private void GetVertices()
+    {
+        jellyVertices = new JellyVertex[mesh.vertices.Length];
+        currentMeshVertices = new Vector3[mesh.vertices.Length];
+        for (int i = 0; i < mesh.vertices.Length; i++)
+        {
+            jellyVertices[i] = new JellyVertex(i, mesh.vertices[i], mesh.vertices[i], Vector3.zero);
+            currentMeshVertices[i] = mesh.vertices[i];
+        }
+    }
+
+    private void Update()
+    {
+        UpdateVertices();
+    }
+
+    private void UpdateVertices()
+    {
+        for (int i = 0; i < jellyVertices.Length; i++)
+        {
+            jellyVertices[i].UpdateVelocity(bounceSpeed);
+            jellyVertices[i].Settle(stiffness);
+
+            jellyVertices[i].currentVertexPos += jellyVertices[i].currentVelocity * Time.deltaTime;
+            currentMeshVertices[i] = jellyVertices[i].currentVertexPos;
+        }
+
+        mesh.vertices = currentMeshVertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        ContactPoint[] collisionPoints = other.contacts;
+        for (int i = 0; i < collisionPoints.Length; i++)
+        {
+            Vector3 inputPoint = collisionPoints[i].point + (collisionPoints[i].point * .1f);
+            ApplyPressureToPoint(inputPoint, fallForce);
+        }
+    }
+
+    public void ApplyPressureToPoint(Vector3 _point,float _pressure)
+    {
+
+        for (int i = 0; i < jellyVertices.Length; i++)
+        {
+            jellyVertices[i].ApplyPressureToVertex(transform, _point, _pressure);
+        }
+
+    }
+}
+
