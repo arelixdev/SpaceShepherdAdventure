0cbcaeb9d09e639d888a1936bf7d00aa071abc47
OwenMdvd <owen.mdvd@gmail.com>
2020-02-25 14:30:45 +0100

    Node/Boo/Sapin

--- Assets/Scripts/CreateNode.cs
+++ Assets/Scripts/CreateNode.cs
@@ -1,116 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Pathfinding;
 
+[ExecuteInEditMode]
 public class CreateNode : MonoBehaviour
 {
-    public Vector3 scaling;
-    public GameObject sapin;
-    public GameObject node;
-    public Transform parentObstacle;
+    public bool spawn;
+    public bool clear;
     Mesh mesh;
-    public float chanceSapin;
     public bool barycentre;
-    public LayerMask layer;
 
-    void Start()
+    void Update()
     {
-        if (parentObstacle != null)
+        if(spawn)
         {
-            parentObstacle.position = transform.position;
-            parentObstacle.localScale = transform.localScale;
-            parentObstacle.rotation = transform.rotation;
-        }
+            spawn = false;
+            ClearListEditor();
 
-        mesh = GetComponent<MeshFilter>().mesh;
+            mesh = GetComponent<MeshFilter>().sharedMesh;
+            GameObject parentNode = new GameObject("Nodes");
+            parentNode.transform.parent = transform;
+            parentNode.transform.localScale = Vector3.one;
+            parentNode.transform.position = Vector3.zero;
 
-        for (int i = 0; i < mesh.vertices.Length; i++)
-        {
-            GameObject sphere = Instantiate(node);
-            sphere.name = i.ToString();
-            sphere.transform.parent = transform;
-            sphere.transform.localScale = scaling;
-            sphere.transform.localPosition = mesh.vertices[i];
-
-
-            float rd = Random.Range(0, 10);
-            if (rd < chanceSapin)
+            for (int i = 0; i < mesh.vertices.Length; i++)
             {
-                GameObject sapinGo = Instantiate(sapin);
-                sapinGo.name = i.ToString();
-                sapinGo.transform.parent = parentObstacle;
-                sapinGo.transform.localPosition = mesh.vertices[i];
-
-                OrientBody(sapinGo.GetComponent<Rigidbody>(), FindSurface(sapinGo.transform));
+                GameObject sphere = new GameObject("Node_" + i.ToString());
+                sphere.transform.parent = parentNode.transform;
+                sphere.transform.localPosition = mesh.vertices[i];
             }
-        }
-        if (barycentre)
-        {
-            int[] triangles = mesh.triangles;
-            for (int i = 0; i < triangles.Length / 3; i++)
+            if (barycentre)
             {
-                Vector3 p0 = mesh.vertices[triangles[i * 3 + 0]];
-                Vector3 p1 = mesh.vertices[triangles[i * 3 + 1]];
-                Vector3 p2 = mesh.vertices[triangles[i * 3 + 2]];
-
-                p0 = transform.TransformPoint(p0);
-                p1 = transform.TransformPoint(p1);
-                p2 = transform.TransformPoint(p2);
-                Vector3 center = ((p0 + p1 + p2) / 3);
-
-                GameObject sphere = Instantiate(node);
-                sphere.name = i.ToString() + "_Centre";
-                sphere.transform.parent = transform;
-                sphere.transform.localScale = scaling;
-                sphere.transform.position = center;
-            }
-        }
-
-        Invoke("DetectUnwalk", .5f);
-    }
-
-    public void DetectUnwalk()
-    {
-        AstarPath.active.Scan();
-
-        PointGraph pg = AstarPath.active.graphs[0] as PointGraph;
-        foreach (var node in pg.nodes)
-        {
-            node.Walkable = true;
-            Collider[] hitColliders = Physics.OverlapSphere((Vector3)node.position, .25f);
-            for (int i = 0; i < hitColliders.Length; i++)
-            {
-                if (hitColliders[i].CompareTag("obstacle"))
+                int[] triangles = mesh.triangles;
+                for (int i = 0; i < triangles.Length / 3; i++)
                 {
-                    node.Walkable = false;
-                }
-                else if (hitColliders[i].CompareTag("water"))
-                {
-                    node.Tag = (1 << 0);
+                    Vector3 p0 = mesh.vertices[triangles[i * 3 + 0]];
+                    Vector3 p1 = mesh.vertices[triangles[i * 3 + 1]];
+                    Vector3 p2 = mesh.vertices[triangles[i * 3 + 2]];
+
+                    p0 = transform.TransformPoint(p0);
+                    p1 = transform.TransformPoint(p1);
+                    p2 = transform.TransformPoint(p2);
+                    Vector3 center = ((p0 + p1 + p2) / 3);
+
+                    GameObject sphere = new GameObject("Node_" + i.ToString() + "_Centre");
+                    sphere.transform.parent = parentNode.transform;
+                    sphere.transform.position = center;
                 }
             }
+            AstarPath.active.Scan();
         }
-    }
-
-    Vector3 FindSurface(Transform tr)
-    {
-        Vector3 dir = transform.position - tr.position;
-        Vector3 startVert = tr.position - dir * .1f;
-        float distance = Vector3.Distance(transform.position, startVert);
-        Vector3 surfaceNorm = Vector3.zero;
-
-        RaycastHit hit;
-        if (Physics.Raycast(startVert, dir, out hit, layer))
+        if(clear)
         {
-            surfaceNorm = hit.normal;
+            clear = false;
+            ClearListEditor();
         }
-        return surfaceNorm;
     }
 
-    void OrientBody(Rigidbody attractedBody, Vector3 surfaceNorm)
+    private void Start()
     {
-        Quaternion targetRotation = Quaternion.FromToRotation(attractedBody.transform.up, surfaceNorm) * attractedBody.rotation;
-        attractedBody.transform.localRotation = targetRotation;
+        if (Application.isPlaying)
+        {
+            ClearList();
+        }
+    }
+
+    public void ClearListEditor()
+    {
+        int childs = transform.childCount;
+        for (int i = childs - 1; i >= 0; i--)
+        {
+            if(transform.GetChild(i).name.Contains("Node"))
+                DestroyImmediate(transform.GetChild(i).gameObject);
+        }
+    }
+    public void ClearList()
+    {
+        int childs = transform.childCount;
+        for (int i = childs - 1; i >= 0; i--)
+        {
+            if (transform.GetChild(i).name.Contains("Node"))
+                Destroy(transform.GetChild(i).gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/NightEnvironnement.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class NightEnvironnement : MonoBehaviour
+{
+    public enum Types { Sapin , Boo};
+    public Types currentType;
+
+
+    //Sapin
+    [DrawIf("currentType", Types.Sapin)] public Transform parent;
+
+    //Boo
+    public List<PointNode> nodes = new List<PointNode>();
+    [HideInInspector] public List<GameObject> sheeps;
+
+    private void Start()
+    {
+        switch (currentType)
+        {
+            case Types.Sapin:
+                transform.parent = parent;
+                break;
+            case Types.Boo:
+                StartCoroutine(MoveTo(.5f, -transform.up, 5, "RefreshNode"));
+                break;
+        }
+    }
+
+    #region Collision
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("night"))
+        {
+            switch(currentType)
+            {
+                case Types.Sapin:
+                    StartCoroutine(MoveTo(1, -transform.up, 5, "RefreshNode"));
+                    break;
+            }
+        }
+        if (other.CompareTag("Player"))
+        {
+            switch (currentType)
+            {
+                case Types.Boo:
+                    if (!sheeps.Contains(other.gameObject))
+                        sheeps.Add(other.gameObject);
+                    if(sheeps.Count < 2)
+                        StartCoroutine(MoveTo(.5f, transform.up, 5, "RefreshNode"));
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("night"))
+        {
+            switch (currentType)
+            {
+                case Types.Sapin:
+                    StartCoroutine(MoveTo(.5f, transform.up, 5 , "RefreshNode"));
+                    break;
+            }
+        }
+        if (other.CompareTag("Player"))
+        {
+            switch (currentType)
+            {
+                case Types.Boo:
+                    sheeps.Remove(other.gameObject);
+                    if (sheeps.Count < 1)
+                    {
+                        StartCoroutine(MoveTo(.5f, -transform.up, 5, "RefreshNode"));
+                    }
+                    break;
+            }
+        }
+    }
+    #endregion
+
+    #region Movment
+    IEnumerator MoveTo(float time, Vector3 dir, float DistMult ,string voidToCall)
+    {
+        Vector3 start = transform.position;
+        Vector3 end = start + dir * DistMult;
+        float t = 0;
+
+        /*
+        while (t < 1)
+        {
+            
+            t += Time.deltaTime / time;
+            transform.position = Vector3.Lerp(start, end, t);
+        }*/
+        transform.position = end;
+        yield return new WaitForSeconds(0.1f);
+
+        if (!string.IsNullOrEmpty(voidToCall))
+        {
+            Invoke(voidToCall, 0);
+        }
+    }
+
+    public void RefreshNode()
+    {
+        foreach (var node in nodes)
+        {
+            Debug.Log((Vector3)node.position);
+            SetNode._instance.NodeInfo(node, false);
+        }
+    }
+    #endregion
+}
