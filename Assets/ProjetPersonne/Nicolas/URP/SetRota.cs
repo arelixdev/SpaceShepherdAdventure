f0fb4c8330897c050570e06f12ec02e84405d741
nicolas lagree <sensisaw6@gmail.com>
2020-02-26 09:53:04 +0100

    Day/Night Cycle

---
+++ Assets/ProjetPersonne/Nicolas/URP/SetRota.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class SetRota : MonoBehaviour
+{
+    public float speed;
+    public bool active;
+    public Material[] mats;
+    public bool resetMat;
+
+    // Rajouter un tableau de materials
+    void Update()
+    {
+        if (active && Application.isPlaying)
+        {
+            float rota = transform.eulerAngles.y + speed * Time.deltaTime;
+            transform.eulerAngles = new Vector3(0, rota, 0);
+        }
+        foreach (Material mat in mats)
+        {
+            mat.SetFloat("Vector1_9B06EB91", -transform.eulerAngles.y);
+        }
+        if(resetMat)
+        {
+            resetMat = false;
+            GetComponent<Renderer>().sharedMaterials = mats;
+        }
+    }
+}
